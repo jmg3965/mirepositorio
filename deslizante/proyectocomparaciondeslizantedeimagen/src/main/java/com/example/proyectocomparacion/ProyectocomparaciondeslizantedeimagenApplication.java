cc7f951cf97bfa34557825b48afb6f5b9dd6b5e2
jmg3965 <46908583+jmg3965@users.noreply.github.com>
2019-06-25 10:27:11 +0200

    Add files via upload

---
+++ deslizante/proyectocomparaciondeslizantedeimagen/src/main/java/com/example/proyectocomparacion/ProyectocomparaciondeslizantedeimagenApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectocomparacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectocomparaciondeslizantedeimagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectocomparaciondeslizantedeimagenApplication.class, args);
+	}
+
+}
