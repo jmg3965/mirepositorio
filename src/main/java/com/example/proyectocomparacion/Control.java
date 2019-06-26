16046b2cb0dfabeea69c246f983b8482e55ef6b5
jmg3965 <46908583+jmg3965@users.noreply.github.com>
2019-06-26 14:04:05 +0200

    Add files via upload

---
+++ src/main/java/com/example/proyectocomparacion/Control.java
@@ -0,0 +1,31 @@
+package com.example.proyectocomparacion;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@Controller
+@ComponentScan
+public class Control extends WebMvcConfigurerAdapter {
+
+    
+    @GetMapping("index")
+    public String primera()
+    {
+        return "index";
+    }
+    //esta parte del código nos permite servir  contenido estático
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+}
